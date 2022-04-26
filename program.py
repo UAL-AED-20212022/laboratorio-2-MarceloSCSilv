f25d3d3ec009aa75a0fa3aa3d13f637a39b8cd32
Marcelo Silva <marcelosilva@MacBook-Pro-de-Marcelo.local>
2022-04-26 20:40:31 +0100

    Lab2

--- program.py
+++ program.py
@@ -1,3 +1,6 @@
+#Sistema de armazenamentoi de nome de países
+#Implementação de um programa que o tulizador possa armazenar e consultar nome de países.
+
 from views import view
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
